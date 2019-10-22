42adf3e1dda8666aeac1e45b4a561318df2abc81
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-10-22 14:17:05 +0200

    Add explicit wait for results to be loaded

--- pages/results_page.go
+++ pages/results_page.go
@@ -1,7 +1,9 @@
 package pages
 
 import (
+	"github.com/tebeka/selenium"
 	"github.com/zoltanNemeth/hahuTesting/driver"
+	"time"
 )
 
 type ResultsPage struct {
@@ -22,5 +24,15 @@ func NewResultsPage(URL string) ResultsPage {
 }
 
 func (r *ResultsPage) GetNumOfResults() int {
+	condition := selenium.Condition(func(wd selenium.WebDriver) (bool, error) {
+		if len(r.page.FindElementsByXpath(resultsXpath)) < 1 {
+			return false, nil
+		}
+		return true, nil
+	})
+	err := driver.Driver().WaitWithTimeoutAndInterval(condition, time.Duration(time.Second*100), time.Duration(time.Second*3))
+	if err != nil {
+		return 0
+	}
 	return len(r.page.FindElementsByXpath(resultsXpath))
 }
