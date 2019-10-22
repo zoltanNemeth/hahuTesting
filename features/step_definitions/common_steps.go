a82c14a1f3d0647ac12e67ad05fc46699a605688
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-10-22 11:59:57 +0200

    Refactor, add TODO
    Refactoring in common_steps.go, error message beautified in
    func userIsOnTheSite()

--- features/step_definitions/common_steps.go
+++ features/step_definitions/common_steps.go
@@ -1,21 +1,23 @@
 package step_definitions
 
 import (
-	"errors"
+	"fmt"
 	"github.com/DATA-DOG/godog"
+	"github.com/zoltanNemeth/hahuTesting/driver"
 	"github.com/zoltanNemeth/hahuTesting/pages"
 )
 
 func userIsOnTheSite() error {
 	pages.HomePage().Page.GoTo()
 	if pages.HomePage().Page.IsAt() {
-		//alma := fmt.Errorf("expected %d godogs to be remaining, but there is %d", "alma", "körte")
-		//fmt.Println(alma)
 		return nil
 	}
-	return errors.New("user is not on the correct site")
+	actual, _ := driver.Driver().CurrentURL()
+	expected := pages.HomePage().Page.URL
+	return fmt.Errorf("user is not on the correct site,\n %v \ninstead of %v", actual, expected)
 }
 
 func CommonSteps(s *godog.Suite) {
 	s.Step(`^user is on the site$`, userIsOnTheSite)
+	// TODO: teardown function for the suite
 }
